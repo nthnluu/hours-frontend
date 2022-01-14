38ef3cf63c7abea576019142e6e52621c7468606
nathanluu <nate1299@me.com>
2022-01-14 01:20:23 -0800

    added bouncing cubes animation component + lottie json

---
+++ src/components/animations/BouncingCubesAnimation/BouncingCubesAnimation.tsx
@@ -0,0 +1,23 @@
+import {FC} from "react";
+import Lottie from "react-lottie";
+import * as animationData from "@util/lottie/86284-bouncing-shapes.json";
+
+export interface BouncingCubesAnimationProps {
+}
+
+const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+};
+
+const BouncingCubesAnimation: FC<BouncingCubesAnimationProps> = ({}) => {
+    return <Lottie options={lottieOptions} height={250} width={200}/>;
+};
+
+export default BouncingCubesAnimation;
+
+
