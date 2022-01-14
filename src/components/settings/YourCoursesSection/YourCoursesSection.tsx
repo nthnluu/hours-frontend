d16733be952101a5692fe97b1da76e142f519288
nathanluu <nate1299@me.com>
2022-01-14 00:10:07 -0800

    refactored YourCoursesSection.tsx to use settings section component

--- src/components/settings/YourCoursesSection/YourCoursesSection.tsx
+++ src/components/settings/YourCoursesSection/YourCoursesSection.tsx
@@ -1,39 +1,27 @@
 import React, {FC} from "react";
-import {Box, List, Paper, Stack, Typography} from "@mui/material";
+import {List} from "@mui/material";
 import {useCourses} from "@util/course/hooks";
 import {useSession} from "@util/auth/hooks";
-import { CoursePermission } from "@util/auth/api";
+import {CoursePermission} from "@util/auth/api";
 import CourseListItem from "../CourseListItem";
+import SettingsSection from "@components/settings/SettingsSection";
 
 export interface YourCoursesSectionProps {
 }
 
 /**
- * Lists your own courses.
+ * Lists courses in which you've been granted admin privileges.
  */
 const YourCoursesSection: FC<YourCoursesSectionProps> = ({}) => {
     const {currentUser, loading} = useSession();
     const [courses, loadingCourses] = useCourses();
 
-    if (loading || loadingCourses) return <></>;
-
-    return (
-        <Paper variant="outlined">
-            <Box p={3}>
-                <Stack direction="row" justifyContent="space-between">
-                    <Typography variant="h5" fontWeight={600}>
-                        Your Courses
-                    </Typography>
-                </Stack>
-
-                {courses && <List>
-                    {courses.filter(course => currentUser?.coursePermissions && (currentUser.coursePermissions[course.id] === CoursePermission.CourseAdmin))
-                            .map(course => <CourseListItem key={course.id} course={course} />)}
-                </List>}
-
-            </Box>
-        </Paper>
-    );
+    return <SettingsSection title="Your courses" loading={loading || loadingCourses}>
+        {courses && <List>
+            {courses.filter(course => currentUser?.coursePermissions && (currentUser.coursePermissions[course.id] === CoursePermission.CourseAdmin))
+                .map(course => <CourseListItem key={course.id} course={course}/>)}
+        </List>}
+    </SettingsSection>;
 };
 
 export default YourCoursesSection;
