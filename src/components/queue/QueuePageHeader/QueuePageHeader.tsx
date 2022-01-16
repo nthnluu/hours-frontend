74b8e52ceae0ca11dc699d615fdfa61780f56f28
nathanluu <nate1299@me.com>
2022-01-16 13:55:09 -0800

    updated QueuePageHeader.tsx styling

--- src/components/queue/QueuePageHeader/QueuePageHeader.tsx
+++ src/components/queue/QueuePageHeader/QueuePageHeader.tsx
@@ -8,12 +8,13 @@ export interface QueuePageHeaderProps {
 
 const QueuePageHeader: FC<QueuePageHeaderProps> = ({queue}) => {
     return <Paper sx={{overflow: "hidden"}}>
-        <Box width="100%" height={225} color="#fff" position="relative" sx={{bgcolor: queue.color ?? "#172c35"}}>
-            <Box position="absolute" bottom={18} left={20}>
+        <Box width="100%" p={3} color="#fff" position="relative" sx={{bgcolor: queue.color ?? "#172c35"}}>
+            <Box height={120}/>
+            <Box>
                 <Typography variant="body1" noWrap>
                     {queue.course.code}: {queue.course.title}
                 </Typography>
-                <Stack direction="row" spacing={1} alignItems="center" mt={1}>
+                <Stack direction="row" spacing={1} alignItems="center">
                     <Typography variant="h4" fontWeight={600}>
                         {queue.title}
                     </Typography>
