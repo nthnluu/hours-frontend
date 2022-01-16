519cc70a45312f0c4fc24ddbc9c1af8a11cdef48
nathanluu <nate1299@me.com>
2022-01-16 14:34:57 -0800

    added adminOnly indicator to SettingsSection.tsx

--- src/components/settings/SettingsSection/SettingsSection.tsx
+++ src/components/settings/SettingsSection/SettingsSection.tsx
@@ -2,6 +2,8 @@ import React, {FC} from "react";
 import {Box, CircularProgress, Paper, Stack, Typography} from "@mui/material";
 import Button from "@components/shared/Button";
 import {useAuth} from "@util/auth/hooks";
+import IconButton from "@components/shared/IconButton";
+import GppGoodIcon from '@mui/icons-material/GppGood';
 
 export interface SettingsSectionProps {
     title: string;
@@ -19,9 +21,15 @@ const SettingsSection: FC<SettingsSectionProps> = ({title, loading, adminOnly, a
     return (adminOnly && currentUser?.isAdmin) || !adminOnly ? <Paper variant="outlined">
         <Box p={3}>
             <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <Typography variant="h6" fontWeight={600}>
-                    {title}
-                </Typography>
+                <Stack direction="row" justifyContent="start" alignItems="center" spacing={0.5}>
+                    <Typography variant="h6" fontWeight={600}>
+                        {title}
+                    </Typography>
+                    {adminOnly && <IconButton label="Only visible to admins" size="small">
+                        <GppGoodIcon sx={{opacity: 0.36}}/>
+                    </IconButton>}
+                </Stack>
+
                 {actionButton && <Button size="small" variant="contained" onClick={actionButton.onClick}>
                     {actionButton.label}
                 </Button>}
