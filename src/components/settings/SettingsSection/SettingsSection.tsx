92a6e1cbbc0f8b0ea2746f1e8a71b17ce21b9be7
nathanluu <nate1299@me.com>
2022-01-16 14:57:52 -0800

    added subtitle prop to SettingsSection.tsx

--- src/components/settings/SettingsSection/SettingsSection.tsx
+++ src/components/settings/SettingsSection/SettingsSection.tsx
@@ -7,6 +7,7 @@ import GppGoodIcon from '@mui/icons-material/GppGood';
 
 export interface SettingsSectionProps {
     title: string;
+    subtitle?: string;
     loading?: boolean;
     adminOnly?: boolean;
     actionButton?: {
@@ -15,19 +16,24 @@ export interface SettingsSectionProps {
     }
 }
 
-const SettingsSection: FC<SettingsSectionProps> = ({title, loading, adminOnly, actionButton, children}) => {
+const SettingsSection: FC<SettingsSectionProps> = ({title, subtitle, loading, adminOnly, actionButton, children}) => {
     const {currentUser} = useAuth();
 
     return (adminOnly && currentUser?.isAdmin) || !adminOnly ? <Paper variant="outlined">
         <Box p={3}>
             <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <Stack direction="row" justifyContent="start" alignItems="center" spacing={0.5}>
-                    <Typography variant="h6" fontWeight={600}>
-                        {title}
+                <Stack mb={subtitle ? 2 : 0}>
+                    <Stack direction="row" justifyContent="start" alignItems="center" spacing={0.5}>
+                        <Typography variant="h6" fontWeight={600}>
+                            {title}
+                        </Typography>
+                        {adminOnly && <IconButton label="Only visible to admins" size="small">
+                            <GppGoodIcon sx={{opacity: 0.36}}/>
+                        </IconButton>}
+                    </Stack>
+                    <Typography variant="body2" sx={{opacity: 0.75}}>
+                        {subtitle}
                     </Typography>
-                    {adminOnly && <IconButton label="Only visible to admins" size="small">
-                        <GppGoodIcon sx={{opacity: 0.36}}/>
-                    </IconButton>}
                 </Stack>
 
                 {actionButton && <Button size="small" variant="contained" onClick={actionButton.onClick}>
