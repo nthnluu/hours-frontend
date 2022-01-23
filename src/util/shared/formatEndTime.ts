3cc9cff0fb1638982fd5be66dee4f7e15ec28fb1
nthnluu <nate1299@me.com>
2022-01-23 14:52:10 -0500

    fixed end time formatting

--- src/util/shared/formatEndTime.ts
+++ src/util/shared/formatEndTime.ts
@@ -1,5 +1,5 @@
 import {format} from "date-fns";
 
 export default function formatEndTime(endTime: Date): string {
-    return format(endTime, `'End${endTime < new Date() ? "ed" : "s"} at' K:m a`);
+    return format(endTime, `'End${endTime < new Date() ? "ed" : "s"} at' K:mm a`);
 }
