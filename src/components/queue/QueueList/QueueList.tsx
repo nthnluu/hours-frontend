29621b0aefc575bdb24f5b94adb2db2061492d8d
Nick Young <youngnicholas228@gmail.com>
2022-01-25 20:20:14 -0500

    a

--- src/components/queue/QueueList/QueueList.tsx
+++ src/components/queue/QueueList/QueueList.tsx
@@ -49,7 +49,7 @@ const QueueList: FC<QueueListProps> = ({queue, showCompletedTickets}) => {
                 <Typography variant="h6" fontWeight={600}>
                     Queue
                 </Typography>
-                {!queue.isCutOff && !queueEnded && !inQueue && //!isTA(queue.course.id) &&
+                {!queue.isCutOff && !queueEnded && !inQueue && !isTA(queue.course.id) &&
                     <Button variant="contained" onClick={() => setCreateTicketDialog(true)}>
                         Join Queue
                     </Button>}
