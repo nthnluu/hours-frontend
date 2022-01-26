5f853b34141c3768a8b651a54349f11b2aa58dc8
nthnluu <nate1299@me.com>
2022-01-26 02:39:52 -0500

    updated queue color

--- src/util/shared/getQueueColor.ts
+++ src/util/shared/getQueueColor.ts
@@ -14,7 +14,7 @@ export default function getQueueColor(queue: Queue): string {
     if (queue.endTime < new Date()) {
         return "#1f1f1f";
     } else {
-        const colors = ["#304ffe", "#ad1457", "#00c853", "#0288d1", "#f50057", "#0097a7", "#aa00ff", "#0288d1"];
+        const colors = ["#3f51b5", "#2196f3", "#673ab7", "#00838f", "#880e4f", "#4a148c", "#b71c1c", "#004d40"];
         const hash = hashCodeFromString((queue.course.id + queue.course.title + queue.title));
         const colorIndex = Math.abs(hash % (colors.length - 1));
         return colors[colorIndex];
