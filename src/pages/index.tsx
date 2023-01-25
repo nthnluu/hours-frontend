e39248c430c8559f4d54bc73ee5a428b7b412a53
Nathan Benavides-Luu <nate1299@me.com>
2023-01-24 22:08:17 -0500

    Queue rejoin cooldown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,14 @@ import CreateQueueDialog from "@components/home/CreateQueueDialog";
 import {useAuth} from "@util/auth/hooks";
 import Button from "@components/shared/Button";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import WhatsNewDialog from "@components/home/WhatsNewDialog";
-
-const WHATS_NEW_KEY = 'whats-new-dismissed';
-const WHATS_NEW_VER = '1';
 
 export default function Home() {
     const {currentUser, isAuthenticated} = useAuth();
     const [queues, loading] = useQueues();
     const [createQueueDialog, setCreateQueueDialog] = useState(false);
-    // const [whatsNewDialog, setWhatsNewDialog] = useState(localStorage.getItem(WHATS_NEW_KEY) != WHATS_NEW_VER);
-    const [whatsNewDialog, setWhatsNewDialog] = useState(false);
-
     const isTA = isAuthenticated && currentUser && currentUser.coursePermissions && (Object.keys(currentUser.coursePermissions).length > 0);
 
-    // Marks the current What's New dialog as read by storing a flag in local storage
-    function markAsRead() {
-        localStorage.setItem(WHATS_NEW_KEY, WHATS_NEW_VER);
-        setWhatsNewDialog(false);
-    }
-
     return <AppLayout maxWidth={false} loading={loading}>
-        {/*{currentUser?.coursePermissions && <WhatsNewDialog open={whatsNewDialog} onClose={markAsRead}/>}*/}
         <CreateQueueDialog open={createQueueDialog} onClose={() => setCreateQueueDialog(false)}/>
         {queues && queues.length > 0 && isTA && <Box mb={2}>
             <Button startIcon={<AddCircleIcon/>} onClick={() => setCreateQueueDialog(true)}>
